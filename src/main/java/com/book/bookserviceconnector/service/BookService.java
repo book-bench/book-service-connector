1abac8c561283a15174531bf751ba16532abf8f4
YuraLukianchenko <qlukianchenkop@gmail.com>
2023-05-02 17:53:26 +0300

    new branch with httpservice implementation

--- src/main/java/com/book/bookserviceconnector/service/BookService.java
+++ src/main/java/com/book/bookserviceconnector/service/BookService.java
@@ -1,45 +1,21 @@
 package com.book.bookserviceconnector.service;
 
-import com.book.bookserviceconnector.model.Book;
 import com.book.bookserviceconnector.model.ResponseFromBookService;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
 
-import java.util.List;
+
+@HttpExchange("/books")
+public interface BookService {
+
+    @GetExchange
+    ResponseFromBookService getAvailableBooks();
+
+    @GetExchange("/{id}")
+    ResponseFromBookService getBookById(@RequestParam int id);
 
 
-public class BookService {
-
-    WebClient webClient;
-
-    public BookService(WebClient webClient) {
-        this.webClient = webClient;
-    }
-
-    public List<Book> getAvailableBooks() {
-
-        ResponseFromBookService response = webClient
-                .get()
-                .uri("/books")
-                .retrieve()
-                .bodyToMono(ResponseFromBookService.class)
-                .block();
-
-        return response.getBooks();
-    }
-
-    public Book getBookById(int id) {
-
-        String path = "/books/" + id;
-
-        ResponseFromBookService response =  webClient
-                .get()
-                .uri(path)
-                .retrieve()
-                .bodyToMono(ResponseFromBookService.class)
-                .block();
-
-        return response.getBooks().get(0);
-    }
 
 
 
